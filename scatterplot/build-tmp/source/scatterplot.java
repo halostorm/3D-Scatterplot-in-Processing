54132545c294bc257fae9eeb65951689def399c4
Laura Cooper <lmcooper90@gmail.com>
2015-06-07 15:21:24 -0400

    cleaning up

--- scatterplot/build-tmp/source/scatterplot.java
+++ scatterplot/build-tmp/source/scatterplot.java
@@ -1,6 +1,22 @@
-//Global variables
-float x = PI/3.0;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class scatterplot extends PApplet {
+
+float x = PI/3.0f;
 float xval, yval, zval;
+
 int rowCount;
 String charac1, charac2, charac3, charac4;
 int i = 0;
@@ -9,10 +25,12 @@ Table table;
 int row;
 String[] arr1, arr2, arr3, arr4;
 
-void setup() {
+public void setup() {
 	size(500,500,OPENGL);
+
 	table = loadTable("status.csv", "header");
 	row = table.getRowCount();
+
 	arr1 = new String[row];
 
 	//populate arrays
@@ -24,18 +42,22 @@ void setup() {
 	}
 
 	i=0;
+	println("row is " + str(row) + "and rowCount is " + str(rowCount));
 
 	arr2 = new String[row];
+
 	for (TableRow row : table.rows()) {
 	    charac2 = row.getString("Percent performing unpaid or community service");
 	    arr2[i] = charac2;
 
 	    i += 1;
+
 	}
 
 	i=0;
 
 	arr3 = new String[row];
+
 	for (TableRow row : table.rows()) {
 	    charac3 = row.getString("At least once a week");
 	    arr3[i] = charac3;
@@ -50,12 +72,23 @@ void setup() {
 	    charac4 = row.getString("At least once a month, but not weekly");
 	    arr4[i] = charac4;
 	    i += 1;
+
 	}
+
  }
   
-void draw() {
+
+
+
+
+
+
+
+	
+
+public void draw() {
 	background(120);
-	x+=0.01;
+	x+=0.01f;
 
 	pushMatrix();
 	translate(width/2,height/2);
@@ -75,6 +108,7 @@ void draw() {
 		float yval = map(communityService,25,65,-300,300);	
 		float zval = map(month,30,45,-300,300);
 
+
 			pushMatrix();
 			noStroke();
 			fill(255,0,0,90);
@@ -82,8 +116,24 @@ void draw() {
 			sphere(5);
 			popMatrix();
 
+
+
 	}
+
+
+
+
+
 
 	popMatrix();
 
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "scatterplot" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
+  }
 }
